4e6b069649c59ca476273a98d4386eee9a454498
Nick Shephard <nickshep9@gmail.com>
2014-02-21 01:56:44 -0800

    Create t1.sh

---
+++ Networking/DV-Algorithm-Simulator/t1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+logsave /tmp/OUTPUT.TXT gnome-terminal -x ./router test1 A
+gnome-terminal -x ./router test1 B
+gnome-terminal -x ./router test1 C
+gnome-terminal -x ./router test1 D
+gnome-terminal -x ./router test1 E
