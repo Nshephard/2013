f23e82462285a96ad8d06bc0e3a7add6aeb9ff39
Nick Shephard <nickshep9@gmail.com>
2015-03-09 01:02:14 -0700

    Delete t1.sh

--- Networking/DV-Algorithm-Simulator/t1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-logsave /tmp/OUTPUT.TXT gnome-terminal -x ./router test1 A
-gnome-terminal -x ./router test1 B
-gnome-terminal -x ./router test1 C
-gnome-terminal -x ./router test1 D
-gnome-terminal -x ./router test1 E
